33f1a08c868461e6b031ed3fd1c6de57fc7c2dc3
Silvia-Du <silviadu@hotmail.it>
2022-10-14 14:18:36 +0200

    Impostato api controller e Prima chiamata api in get

---
+++ Controllers/Api/PizzaController.cs
@@ -0,0 +1,20 @@
+﻿using la_mia_pizzeria_crude_mvc.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        readonly PizzeriaContext _ctx = new();
+
+        [HttpGet]
+        public IActionResult IndexGet()
+        {
+            List<Pizza> pizzas = _ctx.Pizzas?.ToList()!;           
+            return Ok(pizzas);
+        }
+    }
+}
